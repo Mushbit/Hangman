40623d9f82b79e96fcc8cfd16332e5d6a532081f
Max <mushbit@protonmail.com>
2023-07-06 16:07:49 +0200

    placed the previous methods inside of the new Hangman class. Set a few instance variables setting up the game on initialization.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,21 @@
 puts "Hangman Initialized!"
 
-def clean_dictionary(dictionary)
-  puts dictionary.split("\n").keep_if{|a| a.length == 5..12}
+hangman = Hangman.new
+
+class Hangman
+  def initialize
+    @attempts_left = 10
+    @dictionary = clean_dict = clean_dictionary(File.read('google-10000-english.txt'))
+    @word = pick_word(clean_dict)
+    @displayed_word = Array.new(@word.length) {"_"}
+    @incorrect_attempts = []
+  end
+
+  def clean_dictionary(dirty_dict)
+    dirty_dict.split("\n").keep_if{|a| a.length > 4 && a.length < 13}
+  end
+
+  def pick_word(clean_dict)
+    clean_dict[rand(0..clean_dict.length - 1)]
+  end
 end
-clean_dictionary(File.read ('google-10000-english.txt'))
