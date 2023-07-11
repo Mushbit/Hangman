f12f277986c306d90b7d960e48e2ff9b871b2b01
Max <mushbit@protonmail.com>
2023-07-11 11:40:13 +0200

    Uppon Updating the game_state a JSON save file is created. I am yet to make the loading of said file work

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,42 +1,63 @@
 require "pry-byebug"
+require "json"
 
 puts "Hangman Initialized!"
 
-def run
-puts "Press enter to play"
-gets
-# puts "Would you like to continue where you left off? Y/n"
-# answer = gets.chomp.downcase
-# if answer == "y"
-#   Hangman.new(File.open("save.txt", r))
-#   hangman.display_game_progression
-#   hangman.ask_attempt
-# else
-hangman = Hangman.new
-hangman.display_game_progression
-hangman.ask_attempt
-# end
-end
+class Game
+  def self.run
+    puts "Would you like to continue where you left off? Y/n"
+    answer = gets.chomp.downcase
+    if answer == "y"
+      binding.pry
+      Hangman.new(JSON.load("hangman_save.json"))
+      hangman.display_game_progression
+      hangman.ask_attempt
+    else
+      hangman = Hangman.new
+      hangman.display_game_progression
+      hangman.ask_attempt
+    end
+  end
 
-def reset
-  puts 'Want to play again? Y/n'
-  answer = gets.chomp.downcase
-  if answer == 'y'
-    hangman = Hangman.new
-    hangman.display_game_progression
-    hangman.ask_attempt
-  else
-    puts 'Type: "run" if you change your mind.'
+  def self.reset
+    puts 'Want to play again? Y/n'
+    answer = gets.chomp.downcase
+    if answer == 'y'
+      hangman = Hangman.new
+      hangman.display_game_progression
+      hangman.ask_attempt
+    else
+      puts 'Type: "run" if you change your mind.'
+    end
   end
 end
 
+
 class Hangman
   def initialize
-    @dictionary = clean_dict = clean_dictionary(File.read('google-10000-english.txt'))
+    @dictionary = clean_dictionary(File.read('google-10000-english.txt'))
     @word = pick_word(clean_dict)
     @attempts_left = 10
     @correct_attempts = Array.new(@word.length) {"_"}
     @incorrect_attempts = Array.new
+  end
+
+  def to_json
+    JSON.dump ({
+      :dictionary => @dictionary,
+      :word => @word,
+      :attempts_left => @attempts_left,
+      :correct_attempts => @correct_attempts,
+      :incorrect_attempts => @incorrect_attempts
+    })
+  end
+
+  def save_game
+    Dir.mkdir('output') unless Dir.exists?('output')
+    filename = 'hangman_save.json'
+    File.open(filename, 'w') do |file|
+      file.puts to_json
+    end
   end
 
   def clean_dictionary(dirty_dict)
@@ -77,16 +98,17 @@ class Hangman
         ask_attempt
       end
     end
-    display_result(matched_char_indexes, char)
+    update_game_state(matched_char_indexes, char)
   end
 
-  def display_result(matched_char_indexes, char)
+  def update_game_state(matched_char_indexes, char)
     if matched_char_indexes.any?
       matched_char_indexes.each { |index| @correct_attempts[index] = char }
     else
       @incorrect_attempts << char
       @attempts_left -= 1
     end
+    save_game
     display_game_progression
     ask_attempt unless check_win_condition
   end
@@ -94,14 +116,14 @@ class Hangman
   def check_win_condition
     if @correct_attempts.none?('_')
       puts "You win!"
-      reset
+      Game.reset
       true
     elsif@attempts_left <= 0
       puts "You lose!"
-      reset
+      Game.reset
       true
     end
   end
 end
 
-run
+Game.run
