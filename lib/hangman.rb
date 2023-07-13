9228ad8b782a03ecb3b67984a30dbb1a63f7aefe
Max <mushbit@protonmail.com>
2023-07-13 20:10:08 +0200

    Tried to create a central run method that calls all operation methods within Hangman. This fucked some shit up and have been trying to make the app work once more!. I'm close

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,29 +4,46 @@ require "json"
 puts "Hangman Initialized!"
 
 class Game
-  def self.run
+  attr_accessor :hangman
+  def initialize
+    @hangman = Hangman.new
+    launch
+  end
+
+  def launch
     puts "Would you like to continue where you left off? Y/n"
     answer = gets.chomp.downcase
     if answer == "y"
-      binding.pry
-      save_file = File.open('hangman_save.json', 'r')
-      Hangman.new(JSON.load(save_file))
-      hangman.display_game_progression
-      hangman.ask_attempt
-    else
-      hangman = Hangman.new
-      hangman.display_game_progression
-      hangman.ask_attempt
+      save_file = JSON.load(File.read('hangman_save.json'))
+      hangman.load_game(save_file)
+      # have Hangman class accept arguments OR create a self initializing method within the class...interesting!
     end
+    run
   end
 
-  def self.reset
+  def run
+    hangman.display_game_progression
+
+    until hangman.check_game_over_condition do
+      hangman.save_game
+
+      char = hangman.ask_attempt
+      binding.pry
+      # working on it right here
+      matched_char_indexes = hangman.check_attempt_validity(char) unless hangman.check_attempt_validity(char)
+      hangman.update_game_state(matched_char_indexes, char)
+      hangman.display_game_progression
+    end
+
+    reset
+  end
+
+  def reset
     puts 'Want to play again? Y/n'
     answer = gets.chomp.downcase
     if answer == 'y'
-      hangman = Hangman.new
-      hangman.display_game_progression
-      hangman.ask_attempt
+      @hangman = Hangman.new
+      run
     else
       puts 'Type: "run" if you change your mind.'
     end
@@ -59,6 +76,13 @@ class Hangman
     end
   end
 
+  def load_game(save_file)
+    @word = save_file['word']
+    @attempts_left = save_file['attempts_left']
+    @correct_attempts = save_file['correct_attempts']
+    @incorrect_attempts = save_file['incorrect_attempts']
+  end
+
   def clean_dictionary(dirty_dict)
     dirty_dict.split("\n").keep_if{ |a| a.length > 4 && a.length < 13 }
   end
@@ -78,53 +102,47 @@ class Hangman
 
   def ask_attempt
     puts "Choose a letter:"
-    char = check_attempt_validity( gets.chomp.downcase )
+    gets.chomp.downcase
   end
 
+  # Returns false upon failure.
   def check_attempt_validity(char)
     matched_char_indexes = []
-
     @word.split('').each_index do |index|
       if char.match?(/^[a-z]$/)
         if @incorrect_attempts.any?( char ) || @correct_attempts.any?( char )
-          puts "That letter has already been used"
-          ask_attempt
+          puts "That letter has already been used, please try again"
+          return false
         elsif @word[index] == char
           matched_char_indexes << index
         end
       else
         puts "The input may only consist of a single letter, please try again"
-        ask_attempt
+        return false
       end
     end
-    update_game_state(matched_char_indexes, char)
+    matched_char_indexes
   end
 
   def update_game_state(matched_char_indexes, char)
     if matched_char_indexes.any?
+
       matched_char_indexes.each { |index| @correct_attempts[index] = char }
     else
       @incorrect_attempts << char
       @attempts_left -= 1
     end
-    save_game
-    display_game_progression
-    ask_attempt unless check_win_condition
   end
 
-  def check_win_condition
+  def check_game_over_condition
     if @correct_attempts.none?('_')
-      binding.pry
-      puts "You win!"
-      Game.reset
+      puts "\n --- You win! --- \n"
       true
     elsif@attempts_left <= 0
-      binding.pry
-      puts "You lose!"
-      Game.reset
+      puts "\n --- You lose! --- \n"
       true
     end
   end
 end
 
-Game.run
+game = Game.new
---
+++ test.rb
@@ -0,0 +1,9 @@
+
+itr = 1
+
+def test
+[true, false, false, false].sample(1)
+end
+while test == false
+  puts test
+end
